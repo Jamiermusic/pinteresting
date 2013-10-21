14d5d62c6bd7a22170bc12893b37e4c6c6e8ce5e
Jamie Robb <noaim87@gmail.com>
2013-10-21 13:22:57 +0200

    S3 AWS for paperclip heroku =>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,14 @@ Pinteresting::Application.configure do
   # Required for heroku
   # Note to set this to actual host
   config.action_mailer.default_url_options = { :host => 'jamier-pinteresting.com' }
+  
+  # Set paperclip image => AWS S3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
